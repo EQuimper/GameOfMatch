573dd8c56e1cb3e4c86c3eee8ca60e32ccd47a00
EQuimper <quimperemanuel@gmail.com>
2016-04-30 15:14:01 -0600

    Add Webpack Babel etc

--- server.js
+++ server.js
@@ -2,21 +2,23 @@ var express = require('express');
 var request = require('request');
 
 const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT || 3000;
+
 console.log(API_KEY);
 
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static('./dist'));
 
-app.listen(3000, function(err) {
+app.get('/summoner/search', function(req, res) {
+  request(`https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${req.query.q}?api_key=${API_KEY}`)
+    .pipe(res);
+});
+
+app.listen(PORT, function(err) {
   if (err) {
     console.log(err);
   } else {
-    console.log('App listen 3000');
+    console.log(`App listen ${PORT}`);
   }
-});
-
-app.get('/summoner/search', function(req, res) {
-  request(`https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/?q=${req.query.q}?api_key=${API_KEY}`)
-    .pipe(res);
 });
